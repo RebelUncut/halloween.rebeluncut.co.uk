42a4dd3ba22e5dccb151be596c4d08cc80f2edc9
Kevin Lewis <kevin@jellybabi.es>
2015-10-20 18:39:46 +0100

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'megturney',
+    clientId: '0fbcd4c4a9114d3ab6e301c32c932864',
+    template: '<li><a href="{{link}}" target="_blank"><img src="{{image}}"></a></li>'
+});
+feed.run();
+
+function checkSize(){
+    if ($(".wrapper").css("padding") == 0 ){
+    	$(".halloweenBat").hide();
+    }
+}
+
+$(document).ready(function() {
+	$(".more").click(function() {
+		$("ul#instafeed li").addClass("active");
+		$(this).hide();
+	});
+});
+
+$(document).ready(function() {
+    checkSize();
+    $(window).resize(checkSize);
+});
+
+$.fn.halloweenBats({
+    image: '/bats/bats.png', // Path to the image.
+    zIndex: 10000, // The z-index you need.
+    amount: 10, // Bat amount.
+    width: 35, // Image width.
+    height: 20, // Animation frame height.
+    frames: 4, // Amount of animation frames.
+    speed: 20, // Higher value = faster.
+    flickering: 15 // Higher value = slower.
+});
